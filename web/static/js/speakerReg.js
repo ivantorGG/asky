7f63c1619ffa1b9d6493d6c5769a7d00391a9b15
Petya <petr.avedov@yandex.ru>
2026-07-02 12:49:43 +0300

    регистрация

--- web/static/js/speakerReg.js
+++ web/static/js/speakerReg.js
@@ -1,8 +1,15 @@
-function sendRegs(email, password){
-    console.log("Емаил", email)
-    console.log("Пароль", password)
+async function sendRegs(email, password){
+    console.log("Емаил", email);
+    console.log("Пароль", password);
 
     const data = {
-        
-    }
+        email: email,
+        password: password
+    };
+
+    const response = await fetch("http://127.0.0.1:8080/register", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    });
 }
